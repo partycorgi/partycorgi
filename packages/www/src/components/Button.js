185a1d88c7e7c044ce388d8d6469201e42ed3e1a
Ryan Warner <warner.ryan@gmail.com>
2020-01-29 09:45:02 -0500

    Use graphql for hero data

--- packages/www/src/components/Button.js
+++ packages/www/src/components/Button.js
@@ -1,19 +1,14 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import React from 'react'
-
-import Hero from '../components/Hero'
 
 export const buttonStyles = {
   fontSize: '16px',
-  fontWeight: 'body',
   borderRadius: '6px',
   height: '33px',
   padding: '0 35px',
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  color: 'text',
   transition: 'all 300ms ease',
   backgroundColor: 'green',
   color: 'background',
--- packages/www/src/components/Hero.js
+++ packages/www/src/components/Hero.js
@@ -1,37 +1,57 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 
 import { buttonStyles } from './Button'
 import MaxWidth from './MaxWidth'
 
-const discordInviteUrl = 'https://discord.gg/S9Gdagv'
+export default props =>
+  <StaticQuery
+    query={query}
+    render={Hero}
+  />
 
-export default props => (
-  <section
-    sx={{
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'center',
-      width: '100vw',
-    }}
-  >
-    <MaxWidth>
-      <h1
-        sx={{
-          fontWeight: 'semiBold',
-          marginBottom: '32px'
-        }}
-      >
-        We're an inclusive community of content creators helping each other grow.
-      </h1>
-      <a
-        href={discordInviteUrl}
-        target='_blank'
-        sx={buttonStyles}
-      >
-        Join Discord
-      </a>
-    </MaxWidth>
-  </section>
-)
+const Hero = data => {
+  const { description, discordInviteUrl } = data.site.siteMetadata
+
+  return (
+    <section
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100vw',
+      }}
+    >
+      <MaxWidth>
+        <h1
+          sx={{
+            fontWeight: 'semiBold',
+            marginBottom: '32px'
+          }}
+        >
+          {description}
+        </h1>
+        <a
+          href={discordInviteUrl}
+          target='_blank'
+          sx={buttonStyles}
+        >
+          Join Discord
+        </a>
+      </MaxWidth>
+    </section>
+  )
+}
+
+const query = graphql`
+  query SiteDiscordQuery {
+    site {
+      siteMetadata {
+        discordInviteUrl
+        description
+      }
+    }
+  }
+`
