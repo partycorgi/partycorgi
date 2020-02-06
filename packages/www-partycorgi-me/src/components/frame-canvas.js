d7be4c563004c7af26917bed2bda22b54b0125d6
Jason Lengstorf <jason@lengstorf.com>
2020-02-06 10:38:27 -0800

    feat: it’s alive! and fluffy!

---
+++ packages/www-partycorgi-me/src/components/frame-canvas.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+
+const FrameCanvas = React.forwardRef(
+  ({ frameSrc, overlays = [], yOffset = 0 }, ref) => {
+    useEffect(() => {
+      if (!frameSrc) return;
+
+      const canvas = ref.current;
+      const context = canvas.getContext('2d');
+
+      const image = new Image();
+      image.onload = function() {
+        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+        overlays.forEach(src => {
+          const overlay = new Image();
+          overlay.onload = function() {
+            context.drawImage(overlay, 0, yOffset, canvas.width, canvas.height);
+          };
+
+          overlay.src = src;
+        });
+      };
+
+      image.src = frameSrc;
+    }, [ref, frameSrc]);
+
+    return (
+      <canvas ref={ref} id="canvas" width="125" height="125">
+        your canvas loads here
+      </canvas>
+    );
+  }
+);
+
+export default FrameCanvas;
--- packages/www-partycorgi-me/src/components/gif-test.js
+++ packages/www-partycorgi-me/src/components/gif-test.js
@@ -1,5 +1,10 @@
-import React from 'react';
+/** @jsx jsx */
+import { useEffect, useRef, useState } from 'react';
+import { jsx } from 'theme-ui';
 import gifshot from 'gifshot';
+import FrameCanvas from './frame-canvas';
+
+// TODO figure out how to export with transparency
 
 const CORGI_FRAMES = [
   '/party-corgi-step-00.png',
@@ -14,31 +19,78 @@ const CORGI_FRAMES = [
   '/party-corgi-step-09.png',
   '/party-corgi-step-10.png',
   '/party-corgi-step-11.png'
-].reverse();
+];
 
 const GifTest = () => {
-  const [img, setImg] = React.useState();
+  const canvasRef00 = useRef();
+  const canvasRef01 = useRef();
+  const canvasRef02 = useRef();
+  const canvasRef03 = useRef();
+  const canvasRef04 = useRef();
+  const canvasRef05 = useRef();
+  const canvasRef06 = useRef();
+  const canvasRef07 = useRef();
+  const canvasRef08 = useRef();
+  const canvasRef09 = useRef();
+  const canvasRef10 = useRef();
+  const canvasRef11 = useRef();
 
-  const createGIF = () =>
-    gifshot.createGIF(
-      {
-        images: CORGI_FRAMES
-      },
-      function(obj) {
-        if (!obj.error) {
-          var image = obj.image;
-          console.log({ image });
+  // 47,45,42,42,45,46,45,42,43,44,48,48
+  const refArray = [
+    { ref: canvasRef00, yOffset: 0 },
+    { ref: canvasRef01, yOffset: -2 },
+    { ref: canvasRef02, yOffset: -5 },
+    { ref: canvasRef03, yOffset: -5 },
+    { ref: canvasRef04, yOffset: -2 },
+    { ref: canvasRef05, yOffset: -1 },
+    { ref: canvasRef06, yOffset: -2 },
+    { ref: canvasRef07, yOffset: -5 },
+    { ref: canvasRef08, yOffset: -4 },
+    { ref: canvasRef09, yOffset: -3 },
+    { ref: canvasRef10, yOffset: 1 },
+    { ref: canvasRef11, yOffset: 1 }
+  ];
 
-          setImg(image);
-        }
-      }
+  const [img, setImg] = useState();
+
+  const createGIF = () => {
+    const images = refArray.map(({ ref }) =>
+      ref.current.toDataURL('image/png')
     );
+    gifshot.createGIF({ images }, function(obj) {
+      if (!obj.error) {
+        var image = obj.image;
+        console.log({ image });
+
+        setImg(image);
+      }
+    });
+  };
+
+  const overlay = new Image();
+  overlay.src = '/sunglasses-that-are-illegal.png';
 
   return (
-    <>
+    <div sx={{ my: 5, mx: 'auto', maxWidth: 500 }}>
       <button onClick={createGIF}>Create a GIF</button>
-      {img && <img alt="Party Corgi" src={img} />}
-    </>
+      {img && <img sx={{ display: 'block' }} alt="Party Corgi" src={img} />}
+
+      <div sx={{ my: 5 }}></div>
+
+      <div
+        sx={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', my: 4 }}
+      >
+        {CORGI_FRAMES.map((src, i) => (
+          <FrameCanvas
+            key={src}
+            frameSrc={src}
+            overlays={['/party-hat.png']}
+            ref={refArray[i].ref}
+            yOffset={refArray[i].yOffset}
+          />
+        ))}
+      </div>
+    </div>
   );
 };
 
