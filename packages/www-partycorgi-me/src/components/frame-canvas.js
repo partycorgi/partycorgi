0d6c12d0db3c444ba3b4adfe66dd020f5c549d76
Ryan Warner <warner.ryan@gmail.com>
2020-02-08 13:29:20 -0500

    Add env browser and node to eslintrc to fix eslint complaining about new Image()

--- packages/www-partycorgi-me/src/components/frame-canvas.js
+++ packages/www-partycorgi-me/src/components/frame-canvas.js
@@ -1,36 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 
 const FrameCanvas = React.forwardRef(
   ({ frameSrc, overlays = [], yOffset = 0 }, ref) => {
     useEffect(() => {
-      if (!frameSrc) return;
+      if (!frameSrc) return
 
-      const canvas = ref.current;
-      const context = canvas.getContext('2d');
+      const canvas = ref.current
+      const context = canvas.getContext('2d')
 
-      const image = new Image();
+      const image = new Image()
       image.onload = function() {
-        context.drawImage(image, 0, 0, canvas.width, canvas.height);
+        context.drawImage(image, 0, 0, canvas.width, canvas.height)
 
         overlays.forEach(src => {
-          const overlay = new Image();
+          const overlay = new Image()
           overlay.onload = function() {
-            context.drawImage(overlay, 0, yOffset, canvas.width, canvas.height);
-          };
+            context.drawImage(overlay, 0, yOffset, canvas.width, canvas.height)
+          }
 
-          overlay.src = src;
-        });
-      };
+          overlay.src = src
+        })
+      }
 
-      image.src = frameSrc;
-    }, [ref, frameSrc]);
+      image.src = frameSrc
+    }, [ref, frameSrc])
 
     return (
-      <canvas ref={ref} id="canvas" width="125" height="125">
+      <canvas ref={ref} id='canvas' width='125' height='125'>
         your canvas loads here
       </canvas>
-    );
+    )
   }
-);
+)
 
-export default FrameCanvas;
+export default FrameCanvas
