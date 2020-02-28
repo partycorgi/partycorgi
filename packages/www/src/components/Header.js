d1d124890e26cf77f13208553f6fb0e2dff00889
Ryan Warner <warner.ryan@gmail.com>
2020-02-28 13:18:18 -0500

    Refactor the header styling

--- packages/www/src/components/Header.js
+++ packages/www/src/components/Header.js
@@ -13,21 +13,22 @@ export default () => {
     <header sx={{
       display: 'flex',
       justifyContent: 'center',
-      padding: '40px 0',
+      height: '70px',
       position: 'fixed',
+      backgroundColor: 'darkPurple',
       zIndex: 3,
-      width: '100vw'
+      width: '100vw',
+      boxShadow: '0px 4px 25px rgba(0, 0, 0, 0.25), 0px 2px 5px rgba(0, 0, 0, 0.15)'
     }}>
       <MaxWidth>
         <div sx={{
           display: 'flex',
           justifyContent: 'space-between',
           alignItems: 'center',
-          backgroundColor: 'lightPurple',
           borderRadius: '10px',
           padding: '8px 20px',
-          height: '56px',
-          boxShadow: '0px 4px 25px rgba(0, 0, 0, 0.25), 0px 2px 5px rgba(0, 0, 0, 0.15)'
+          height: '100%',
+          position: 'relative'
         }}>
           <Wordmark />
           <Nav />
