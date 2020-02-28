6db6ebe04240f04e4aff023d8a5f3686aaae3373
Ryan Warner <warner.ryan@gmail.com>
2020-02-28 10:37:47 -0500

    Create overlay component and wire up to context

--- packages/www/src/layouts/index.js
+++ packages/www/src/layouts/index.js
@@ -3,18 +3,13 @@ import { jsx } from 'theme-ui'
 import React from 'react'
 
 import Header from '../components/Header'
+import Overlay from '../components/Overlay'
 import { StateProvider } from '../store.js'
 
 export default ({ children }) => (
   <StateProvider>
     <Header />
-    <div sx={{
-      width: '100vw',
-      backgroundColor: 'rgba(0, 0, 0, 0.7)',
-      height: '100%',
-      position: 'absolute',
-      zIndex: 2
-    }} />
+    <Overlay />
     <main>
       {children}
     </main>
