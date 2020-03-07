af168b4df4e71d831f4a6c6772a865243cbbe19e
Ryan Warner <warner.ryan@gmail.com>
2020-03-06 20:44:31 -0500

    Add gatsby-plugin-react-helmet to gatsby config

--- packages/www/gatsby-config.js
+++ packages/www/gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
     'gatsby-source-discord',
     'gatsby-plugin-theme-ui',
     'gatsby-plugin-layout',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-google-fonts',
       options: {
