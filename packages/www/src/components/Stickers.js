ac9f183cb0b22a869258a5468fe920ff304eb0c3
Jason Lengstorf <jason@lengstorf.com>
2020-03-24 11:41:12 -0700

    fix: I am a doofus

--- packages/www/src/components/Stickers.js
+++ packages/www/src/components/Stickers.js
@@ -1,20 +1,24 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { loadStripe } from '@stripe/stripe-js';
-import axios from 'axios';
+import { loadStripe } from '@stripe/stripe-js'
+import axios from 'axios'
 
 import CapsTitle from './CapsTitle'
 
 export default () => {
-  const handleClick = event => {
+  const handleSubmit = async event => {
     event.preventDefault()
 
-    const { sessionId } = await axios.post('/.netlify/functions/create-checkout');
+    const { sessionId } = await axios.post(
+      '/.netlify/functions/create-checkout'
+    )
 
     // TODO probably env var this
-    const stripe = await loadStripe('pk_test_38mtS6edMLcrnhLxmoAbTa1S00zC8bvrmi')
+    const stripe = await loadStripe(
+      'pk_test_38mtS6edMLcrnhLxmoAbTa1S00zC8bvrmi'
+    )
     const { error } = await stripe.redirectToCheckout({
-      sessionId,
+      sessionId
     })
 
     alert(error.message)
@@ -27,7 +31,11 @@ export default () => {
       }}
     >
       <CapsTitle>Stickers</CapsTitle>
-      <form action='/.netlify/functions/create-checkout' method='POST' handleSubmit={handleSubmit}>
+      <form
+        action='/.netlify/functions/create-checkout'
+        method='POST'
+        handleSubmit={handleSubmit}
+      >
         <button type='submit'>Buy Stickers</button>
       </form>
       <p>This is currently a test — it doesn’t work yet!</p>
