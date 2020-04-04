2bde38957a28fb02a7e5196e03a942b70b8e33d8
Ryan Warner <warner.ryan@gmail.com>
2020-04-04 16:22:20 -0400

    Proof of concept for twitch function

---
+++ packages/www/src/components/StreamerCard.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import React from 'react'
+
+export default () => {
+  return (
+    <li sx={{
+      width: ['100%']
+    }}>
+      <p></p>
+    </li>
+  )
+}
