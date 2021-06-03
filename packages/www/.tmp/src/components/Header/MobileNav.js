ee44b592011da50d8134114eaeafd498adc4d4bb
Alex Low <me@alexlow.dev>
2021-06-03 14:13:52 -0400

    removed: .tmp dir

--- packages/www/.tmp/src/components/Header/MobileNav.js
+++
@@ -1,5 +0,0 @@
-import { h } from 'preact';
-const MobileNav = ()=>{
-    return h("div", null);
-};
-export default MobileNav;
--- packages/www/.tmp/src/components/Logo/Wordmark.js
+++
@@ -1,7 +0,0 @@
-import { h } from 'preact';
-const Wordmark = ()=>{
-    return h("p", {
-        className: 'font-bold'
-    }, "Party Corgi Network");
-};
-export default Wordmark;
