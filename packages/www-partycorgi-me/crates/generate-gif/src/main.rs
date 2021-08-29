05c5700131f5be46c042d9b6d07047797218d599
ChristopherBiscardi <chris@christopherbiscardi.com>
2021-08-28 20:47:00 -0700

    rust function test

---
+++ packages/www-partycorgi-me/crates/generate-gif/src/main.rs
@@ -0,0 +1,14 @@
+use lamedh_http::{
+    lambda::{lambda, Context, Error},
+    IntoResponse, Request,
+};
+
+#[lambda(http)]
+#[tokio::main]
+async fn main(
+    _: Request,
+    _: Context,
+) -> Result<impl IntoResponse, Error> {
+    dbg!("in main");
+    Ok("boop")
+}
